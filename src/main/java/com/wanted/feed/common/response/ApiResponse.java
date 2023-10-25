0c74206202b306edc8d1d3ffc6c3462314f903ee
min050410 <min050410@gmail.com>
2023-10-25 20:34:22 +0900

    Init: 초기 세팅

---
+++ src/main/java/com/wanted/feed/common/response/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.wanted.feed.common.response;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiResponse<T> {
+    private int status;
+    private T data;
+
+    public ApiResponse() {
+    }
+
+    public ApiResponse(int status) {
+        this.status = status;
+    }
+
+    public ApiResponse(int status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> toResponse(T data) {
+        return new ApiResponse<T>(HttpStatus.OK.value(), data);
+    }
+
+    public static <T> ApiResponse<T> ok() {
+        return new ApiResponse<T>(HttpStatus.NO_CONTENT.value());
+    }
+
+}
---
+++ src/test/java/com/wanted/feed/FeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
