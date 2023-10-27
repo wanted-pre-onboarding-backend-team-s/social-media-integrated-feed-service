05b28f5e319f4890c2ec34b699027e87de7ece3f
newnyee <mebbun0615@gmail.com>
2023-10-27 18:56:55 +0900

    Refactor: SecurityConfig 메소드 변경, 추가

--- src/main/java/com/wanted/feed/common/config/swagger/SecurityConfig.java
+++ src/main/java/com/wanted/feed/common/config/swagger/SecurityConfig.java
@@ -1,28 +1,70 @@
 package com.wanted.feed.common.config.swagger;
 
+import com.wanted.feed.common.JwtFilter;
+import com.wanted.feed.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserRepository userRepository;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
+/*    @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers(
                 "/v3/api-docs",
                 "/v3/api-docs/**",
                 "/swagger-ui.html",
                 "/swagger-ui/**",
-                "/join");
+                "/join",
+                "/login");
+    }*/
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        httpSecurity.sessionManagement(
+                sessionManager -> sessionManager
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        httpSecurity.authorizeHttpRequests(
+                authorize -> authorize
+                        .requestMatchers(
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/join",
+                                "/login").permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        httpSecurity.addFilterBefore(new JwtFilter(userRepository, secretKey), UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
     }
 }
