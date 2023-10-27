1471f0b388b20c5bc266c01f4b9653a746973c2f
min050410 <min050410@gmail.com>
2023-10-27 18:12:49 +0900

    Feature: 피드 상세 조회 엔드포인트 작성

---
+++ src/main/java/com/wanted/feed/feed/controller/FeedController.java
@@ -0,0 +1,27 @@
+package com.wanted.feed.feed.controller;
+
+import com.wanted.feed.common.response.ApiResponse;
+import com.wanted.feed.feed.dto.FeedDetailResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "피드")
+@RestController
+@RequestMapping("/feed")
+@RequiredArgsConstructor
+public class FeedController {
+
+    @Operation(summary = "피드 상세 조회")
+    @GetMapping("/detail/{id}")
+    public ApiResponse<FeedDetailResponseDto> findFeedDetail(
+        @PathVariable Long id
+    ) {
+//        return ApiResponse.toResponse();
+    }
+
+}
