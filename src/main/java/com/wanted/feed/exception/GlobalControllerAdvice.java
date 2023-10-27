9577414bd82b28f953484498a90ca553b242ce1b
newnyee <mebbun0615@gmail.com>
2023-10-27 10:28:01 +0900

    Refactor: 예외 Handler 코드 추가

--- src/main/java/com/wanted/feed/exception/GlobalControllerAdvice.java
+++ src/main/java/com/wanted/feed/exception/GlobalControllerAdvice.java
@@ -1,11 +1,11 @@
 package com.wanted.feed.exception;
 
-import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,11 +14,48 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalControllerAdvice {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> methodArgumentValidExceptionHandler(MethodArgumentNotValidException e) {
+        log.error("[methodArgumentValidExceptionHandler] ex", e);
+        String message = getMessage(e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of("BAD_REQUEST", message));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ErrorResponse> httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException e) {
+        log.error("[httpMessageNotReadableExceptionHandler] ex", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of("BAD_REQUEST", e.getMessage()));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> httpRequestMethodNotSupportedExceptionHandler(HttpRequestMethodNotSupportedException e) {
+        log.error("[HttpRequestMethodNotSupportedException] ex", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of("BAD_REQUEST", e.getMessage()));
+    }
+
     @ExceptionHandler(WantedException.class)
-    public ResponseEntity<ErrorResponse> handleWantedException(WantedException e) {
+    protected ResponseEntity<ErrorResponse> handleWantedException(WantedException e) {
         ErrorType errorType = e.getErrorType();
         log.info("[error] type: {}, sts: {}, msg: {}", errorType.getClassType().getSimpleName(), errorType.getHttpStatus(), errorType.getMessage());
         return ResponseEntity.status(e.getErrorType().getHttpStatus()).body(ErrorResponse.of(e.getErrorType()));
     }
 
+    private String getMessage(MethodArgumentNotValidException e) {
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean isFirst = true;
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            if (!isFirst) {
+                stringBuilder.append(", ");
+            } else {
+                isFirst = false;
+            }
+
+            stringBuilder.append("[");
+            stringBuilder.append(fieldError.getField());
+            stringBuilder.append("]은(는) ");
+            stringBuilder.append(fieldError.getDefaultMessage());
+        }
+        return stringBuilder.toString();
+    }
 }
