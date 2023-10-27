43891bcfb9ef1fbfca4741d74969f742f7078dd7
kmss6905 <kmss69052@naver.com>
2023-10-28 03:19:17 +0900

    Test: SNS 서비스 클라이언트 테스트 추가
    - SnsType 테스트 추가

---
+++ src/test/java/com/wanted/feed/feign/SocialMediaClientTest.java
@@ -0,0 +1,39 @@
+package com.wanted.feed.feign;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.wanted.feed.exception.client.SnsContentIdNotNullException;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+
+@SpringBootTest
+class SocialMediaClientTest {
+
+    @Autowired
+    List<SocialMediaClient> clients;
+
+    @Test
+    @DisplayName("외부로 나가는 모든 SNS 좋아요 요청 응답의 상태값은 200")
+    void likeFeedTest() {
+        List<HttpStatusCode> resultCodes = clients.stream()
+                .map(client -> client.likeFeed("contentId").getStatusCode())
+                .toList();
+
+        assertThat(resultCodes).containsOnly(HttpStatusCode.valueOf(200));
+    }
+
+    @Test
+    @DisplayName("외부 SNS 좋아요 요청 시 contentId 가 Null 이면 예외를 던진다.")
+    void throwExceptionWhenContentIdIsNull() {
+        for (SocialMediaClient client : clients) {
+            assertThatThrownBy(() -> client.likeFeed(null))
+                    .isInstanceOf(SnsContentIdNotNullException.class);
+        }
+    }
+}
