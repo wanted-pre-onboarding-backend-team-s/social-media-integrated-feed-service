66b5c74b8c43b46bf3d5365c3a80cf2d0fc189c7
newnyee <mebbun0615@gmail.com>
2023-10-27 13:47:20 +0900

    Feature: UserRepository 메소드 추가

--- src/main/java/com/wanted/feed/user/domain/UserRepository.java
+++ src/main/java/com/wanted/feed/user/domain/UserRepository.java
@@ -1,8 +1,11 @@
 package com.wanted.feed.user.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
