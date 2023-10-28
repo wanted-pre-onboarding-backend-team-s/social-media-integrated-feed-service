d581abb9f86a7b1a2c23f8644c373a3abe5e9e30
min050410 <min050410@gmail.com>
2023-10-28 22:04:53 +0900

    Style: FeedServiceTest 컨벤션 수정

--- src/test/java/com/wanted/feed/feed/service/FeedServiceTest.java
+++ src/test/java/com/wanted/feed/feed/service/FeedServiceTest.java
@@ -21,15 +21,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class FeedServiceTest {
 
     private static final Long FEED_ID = 1L;
-
     private static final String FEED_TYPE = "spring";
-
     private static final String FEED_TITLE = "피드제목입니다";
-
     private static final int FEED_VIEW_COUNT = 1;
-
     private static final String FEED_CONTENT = "콘텐츠입니다";
-
     private static final String FEED_CONTENT_ID = "1E1D1F";
 
     @InjectMocks
