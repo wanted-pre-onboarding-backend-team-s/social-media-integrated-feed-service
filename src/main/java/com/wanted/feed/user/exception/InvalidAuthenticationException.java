2e18d490a351332b2931ab1d97a80bcdaf887a4b
newnyee <mebbun0615@gmail.com>
2023-10-28 21:48:32 +0900

    Refactor: 사용하지 않는 Exception 제거

--- src/main/java/com/wanted/feed/user/exception/InvalidAuthenticationException.java
+++
@@ -1,5 +0,0 @@
-package com.wanted.feed.user.exception;
-
-import com.wanted.feed.exception.WantedException;
-
-public class InvalidAuthenticationException extends WantedException {}
--- src/main/java/com/wanted/feed/user/exception/TokenValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.wanted.feed.user.exception;
-
-public class TokenValidationException extends RuntimeException{
-
-    public TokenValidationException(String message) {
-        super(message);
-    }
-}
