30ad78db58aaf7a0760f8426fff04a61e6bddef4
min050410 <min050410@gmail.com>
2023-10-28 21:36:05 +0900

    Refactor: BaseCreateTimeEntity 분리

--- src/main/java/com/wanted/feed/common/domain/entity/BaseCreateTimeEntity.java
+++ src/main/java/com/wanted/feed/common/domain/entity/BaseCreateTimeEntity.java
@@ -4,15 +4,15 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTimeEntity extends BaseCreateTimeEntity {
+public class BaseCreateTimeEntity {
 
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
+    @CreatedDate
+    private LocalDateTime createDate;
 
 }
