9c641e589b8929f662f89f7722f15db11daae933
min050410 <min050410@gmail.com>
2023-10-30 02:55:30 +0900

    Feature: 피드 목록 조회 엔드포인트 생성

--- src/main/java/com/wanted/feed/feed/controller/FeedController.java
+++ src/main/java/com/wanted/feed/feed/controller/FeedController.java
@@ -1,12 +1,18 @@
 package com.wanted.feed.feed.controller;
 
 import com.wanted.feed.common.response.ApiResponse;
+import com.wanted.feed.common.response.PagedResponse;
 import com.wanted.feed.feed.dto.FeedDetailResponseDto;
+import com.wanted.feed.feed.dto.FeedResponseDto;
+import com.wanted.feed.feed.dto.SearchFeedRequestDto;
 import com.wanted.feed.feed.service.FeedService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +25,15 @@ public class FeedController {
 
     private final FeedService feedService;
 
+    @Operation(summary = "피드 목록 조회")
+    @GetMapping("/filter")
+    public ApiResponse<PagedResponse<FeedResponseDto>> findFeedsBySearch(
+        @Valid @ParameterObject @ModelAttribute SearchFeedRequestDto searchFeedRequest
+    ) {
+        return ApiResponse.toResponse(feedService.findFeedsBySearch(searchFeedRequest));
+    }
+
+    // TODO:: 피드 상세조회시 HashTag 및 likeCount, shareConnt 추가
     @Operation(summary = "피드 상세 조회")
     @GetMapping("/detail/{id}")
     public ApiResponse<FeedDetailResponseDto> findFeedDetail(
