92c981b59dfb2b7d1cc3a2f2b1b858faf17cab10
Inwoo Hwang <hsjkdss228@naver.com>
2023-10-30 22:11:49 +0900

    Test: getFeedStats 테스트
    - 예외처리, 주요 데이터 변환 로직 검증
    - Mock 데이터 생성을 위한 @Builder, 생성자 적용 방식 수정
      - count 계열 필드들의 경우 특정 값이 전달되면 해당 값을,
        그렇지 않을 경우 0을 할당
      - createdAt 할당을 위한 BaseCreateTimeEntity 내 생성자 추가

--- src/main/java/com/wanted/feed/common/domain/entity/BaseTimeEntity.java
+++ src/main/java/com/wanted/feed/common/domain/entity/BaseTimeEntity.java
@@ -3,10 +3,13 @@ package com.wanted.feed.common.domain.entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -15,4 +18,7 @@ public class BaseTimeEntity extends BaseCreateTimeEntity {
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    protected BaseTimeEntity(LocalDateTime createdAt) {
+        super(createdAt);
+    }
 }
--- src/test/java/com/wanted/feed/feed/service/FeedServiceTest.java
+++ src/test/java/com/wanted/feed/feed/service/FeedServiceTest.java
@@ -2,15 +2,29 @@ package com.wanted.feed.feed.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doReturn;
 
 import com.wanted.feed.common.exception.ErrorType;
 import com.wanted.feed.feed.domain.Feed;
 import com.wanted.feed.feed.domain.FeedRepository;
+import com.wanted.feed.feed.domain.HashtagRepository;
 import com.wanted.feed.feed.dto.FeedDetailResponseDto;
+import com.wanted.feed.feed.dto.StatRequestParamDto;
+import com.wanted.feed.feed.dto.StatResponseDto;
+import com.wanted.feed.feed.exception.DateRangeExceeds30Days;
+import com.wanted.feed.feed.exception.DateRangeExceeds7Days;
 import com.wanted.feed.feed.exception.FeedNotFoundException;
+import com.wanted.feed.feed.exception.StartIsAfterEndException;
+import com.wanted.feed.user.domain.User;
+import com.wanted.feed.user.domain.UserRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,68 +34,269 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class FeedServiceTest {
 
-    private static final Long FEED_ID = 1L;
-    private static final String FEED_TYPE = "spring";
-    private static final String FEED_TITLE = "피드제목입니다";
-    private static final int FEED_DEFAULT_VIEW_COUNT = 0;
-    private static final String FEED_CONTENT = "콘텐츠입니다";
-
-    private static final String FEED_CONTENT_ID = "1E1D1F";
-
     @InjectMocks
     private FeedService feedService;
 
     @Mock
     private FeedRepository feedRepository;
 
-    @DisplayName("피드 상세 조회 실패 존재하지 않음")
-    @Test
-    void find_feed_detail_failed_not_found() {
-        doReturn(Optional.empty()).when(feedRepository).findById(FEED_ID);
+    @Mock
+    private HashtagRepository hashtagRepository;
 
-        final FeedNotFoundException result = assertThrows(
-            FeedNotFoundException.class,
-            () -> feedService.findFeedDetail(FEED_ID)
-        );
+    @Mock
+    private UserRepository userRepository;
 
-        assertThat(result.getErrorType()).isEqualTo(ErrorType.F001);
+    @DisplayName("findFeedDetail")
+    @Nested
+    class FindFeedDetail {
+
+        private static final Long FEED_ID = 1L;
+        private static final String FEED_TYPE = "spring";
+        private static final String FEED_TITLE = "피드제목입니다";
+        private static final int FEED_DEFAULT_VIEW_COUNT = 0;
+        private static final String FEED_CONTENT = "콘텐츠입니다";
+
+        private static final String FEED_CONTENT_ID = "1E1D1F";
+
+        @DisplayName("피드 상세 조회 실패 존재하지 않음")
+        @Test
+        void find_feed_detail_failed_not_found() {
+            doReturn(Optional.empty()).when(feedRepository).findById(FEED_ID);
+
+            final FeedNotFoundException result = assertThrows(
+                    FeedNotFoundException.class,
+                    () -> feedService.findFeedDetail(FEED_ID)
+            );
+
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.F001);
+        }
+
+        @DisplayName("피드 상세 조회 성공")
+        @Test
+        void find_feed_detail_success() {
+            Feed feed = Feed.builder()
+                    .type(FEED_TYPE)
+                    .title(FEED_TITLE)
+                    .content(FEED_CONTENT)
+                    .contentId(FEED_CONTENT_ID)
+                    .build();
+
+            doReturn(Optional.of(feed)).when(feedRepository).findById(FEED_ID);
+
+            final FeedDetailResponseDto result = feedService.findFeedDetail(FEED_ID);
+
+            assertThat(result.getType()).isEqualTo(FEED_TYPE);
+            assertThat(result.getTitle()).isEqualTo(FEED_TITLE);
+            assertThat(result.getContent()).isEqualTo(FEED_CONTENT);
+            assertThat(result.getContentId()).isEqualTo(FEED_CONTENT_ID);
+        }
+
+        @DisplayName("피드 상세 조회시 조회수 1 증가")
+        @Test
+        void find_feed_detail_update_view_count() {
+            Feed feed = Feed.builder()
+                    .type(FEED_TYPE)
+                    .title(FEED_TITLE)
+                    .content(FEED_CONTENT)
+                    .contentId(FEED_CONTENT_ID)
+                    .build();
+
+            doReturn(Optional.of(feed)).when(feedRepository).findById(FEED_ID);
+
+            final FeedDetailResponseDto result = feedService.findFeedDetail(FEED_ID);
+
+            assertThat(result.getViewCount()).isEqualTo(FEED_DEFAULT_VIEW_COUNT + 1);
+        }
     }
 
-    @DisplayName("피드 상세 조회 성공")
-    @Test
-    void find_feed_detail_success() {
-        Feed feed = Feed.builder()
-            .type(FEED_TYPE)
-            .title(FEED_TITLE)
-            .content(FEED_CONTENT)
-            .contentId(FEED_CONTENT_ID)
-            .build();
+    @DisplayName("getFeedStats")
+    @Nested
+    class GetFeedStats {
 
-        doReturn(Optional.of(feed)).when(feedRepository).findById(FEED_ID);
+        private static final Long USER_ID = 1L;
+        private static final String USERNAME = "username";
 
-        final FeedDetailResponseDto result = feedService.findFeedDetail(FEED_ID);
+        @DisplayName("메서드")
+        @Nested
+        class Methods {
 
-        assertThat(result.getType()).isEqualTo(FEED_TYPE);
-        assertThat(result.getTitle()).isEqualTo(FEED_TITLE);
-        assertThat(result.getContent()).isEqualTo(FEED_CONTENT);
-        assertThat(result.getContentId()).isEqualTo(FEED_CONTENT_ID);
+            @DisplayName("toDateTimeToFeeds")
+            @Nested
+            class ToDateTimeToFeeds {
+
+                @DisplayName("List<Feed>를 Map<날짜, List<Feed>>로 변환")
+                @Test
+                void toDateToFeeds() {
+                    List<Feed> feeds = List.of(
+                            Feed.builder().createdAt(LocalDateTime.of(2023, 10, 15, 1, 0, 0))
+                                    .build(),
+                            Feed.builder().createdAt(LocalDateTime.of(2023, 10, 15, 16, 0, 0))
+                                    .build(),
+                            Feed.builder().createdAt(LocalDateTime.of(2023, 10, 15, 23, 50, 0))
+                                    .build(),
+                            Feed.builder().createdAt(LocalDateTime.of(2023, 10, 16, 12, 0, 0))
+                                    .build()
+                    );
+                    String type = "date";
+                    LocalDateTime start = LocalDateTime.of(2023, 10, 15, 0, 0, 0);
+                    LocalDateTime end = LocalDateTime.of(2023, 10, 16, 23, 59, 59);
+
+                    Map<LocalDateTime, List<Feed>> dateToFeeds = feedService
+                            .toDateTimeToFeeds(feeds, type, start, end);
+
+                    assertThat(dateToFeeds).isNotNull();
+                    assertThat(dateToFeeds).hasSize(2);
+
+                    assertThat(dateToFeeds.get(LocalDateTime.of(2023, 10, 15, 0, 0, 0))).hasSize(3);
+                    assertThat(dateToFeeds.get(LocalDateTime.of(2023, 10, 16, 0, 0, 0))).hasSize(1);
+                }
+
+                @DisplayName("List<Feed>를 Map<시간, List<Feed>>로 변환")
+                @Test
+                void toHourToFeeds() {
+                    List<Feed> feeds = List.of();
+                    String type = "hour";
+                    LocalDateTime start = LocalDateTime.of(2023, 10, 15, 0, 0, 0);
+                    LocalDateTime end = LocalDateTime.of(2023, 10, 16, 23, 59, 59);
+
+                    Map<LocalDateTime, List<Feed>> dateToFeeds = feedService
+                            .toDateTimeToFeeds(feeds, type, start, end);
+
+                    assertThat(dateToFeeds).isNotNull();
+                    assertThat(dateToFeeds).hasSize(48);
+                }
+            }
+
+            @DisplayName("toDateTimeToStatResponseDtos")
+            @Nested
+            class ToDateTimeToStatResponseDtos {
+
+                @DisplayName("Map<시간, List<Feed>>를 Map<\"yyyy-mm-dd hh:mm\", StatResponseDto>로 변환")
+                @Test
+                void toHourToStatResponseDtos() {
+                    Map<LocalDateTime, List<Feed>> hourToFeeds = Map.of(
+                            LocalDateTime.of(2023, 10, 15, 14, 0, 0), List.of(
+                                    Feed.builder()
+                                            .viewCount(10).shareCount(2).likeCount(3)
+                                            .createdAt(LocalDateTime.of(2023, 10, 15, 14, 30, 0))
+                                            .build(),
+                                    Feed.builder()
+                                            .viewCount(10).shareCount(2).likeCount(3)
+                                            .createdAt(LocalDateTime.of(2023, 10, 21, 14, 50, 0))
+                                            .build()
+                            ),
+                            LocalDateTime.of(2023, 10, 15, 15, 0, 0), List.of(Feed.builder()
+                                    .viewCount(10).shareCount(2).likeCount(3)
+                                    .createdAt(LocalDateTime.of(2023, 10, 21, 15, 30, 0))
+                                    .build()
+                            )
+                    );
+                    String type = "hour";
+                    List<String> valueSelectors = List.of("count", "share_count");
+
+                    Map<String, StatResponseDto> hourToStatResponseDto = feedService
+                            .toDateTimeToStatResponseDtos(hourToFeeds, type, valueSelectors);
+
+                    assertThat(hourToStatResponseDto).isNotNull();
+                    StatResponseDto first = hourToStatResponseDto.get("2023-10-15 14:00");
+                    assertThat(first.getCount()).isEqualTo(2);
+                    assertThat(first.getViewCount()).isNull();
+                    assertThat(first.getShareCount()).isEqualTo(4);
+                    assertThat(first.getLikeCount()).isNull();
+
+                    StatResponseDto second = hourToStatResponseDto.get("2023-10-15 15:00");
+                    assertThat(second.getCount()).isEqualTo(1);
+                    assertThat(second.getShareCount()).isEqualTo(2);
+                }
+            }
+        }
+
+        @DisplayName("RequestParam 정상")
+        @Nested
+        class ValidRequestParams {
+
+            @DisplayName("Map<\"yyyy-mm-dd\", StatResponseDto> 반환")
+            @Test
+            void getFeedStats() {
+                User user = User.builder()
+                        .username(USERNAME)
+                        .build();
+                given(userRepository.findById(USER_ID))
+                        .willReturn(Optional.of(user));
+
+                List<Feed> feeds = List.of(
+                        Feed.builder()
+                                .viewCount(10).shareCount(2).likeCount(2)
+                                .createdAt(LocalDateTime.of(2023, 10, 18, 16, 0, 0))
+                                .build(),
+                        Feed.builder()
+                                .viewCount(10).shareCount(2).likeCount(2)
+                                .createdAt(LocalDateTime.of(2023, 10, 21, 23, 50, 0))
+                                .build(),
+                        Feed.builder()
+                                .viewCount(10).shareCount(2).likeCount(2)
+                                .createdAt(LocalDateTime.of(2023, 10, 21, 23, 50, 0))
+                                .build()
+                );
+                LocalDateTime start = LocalDateTime.of(2023, 10, 16, 0, 0, 0);
+                LocalDateTime end = LocalDateTime.of(2023, 10, 22, 23, 59, 59);
+                given(feedRepository
+                        .findAllByHashtagAndCreatedAtBetweenStartAndEnd(USERNAME, start, end))
+                        .willReturn(feeds);
+
+                StatRequestParamDto statRequestParamDto = StatRequestParamDto.builder()
+                        .type("date")
+                        .start(start.toLocalDate())
+                        .end(end.toLocalDate())
+                        .value(List.of("count", "view_count"))
+                        .build();
+                Map<String, StatResponseDto> dateToStatResponseDtos = feedService
+                        .getFeedStats(USER_ID, statRequestParamDto);
+
+                assertThat(dateToStatResponseDtos).isNotNull();
+            }
+        }
+
+        @DisplayName("잘못된 RequestParam 존재")
+        @Nested
+        class InvalidRequestParams {
+            @DisplayName("시작일이 종료일보다 앞설 경우 예외 발생")
+            @Test
+            void throwStartIsAfterEndException() {
+                StatRequestParamDto statRequestParamDto = StatRequestParamDto.builder()
+                        .start(LocalDate.of(2023, 10, 15))
+                        .end(LocalDate.of(2023, 10, 12))
+                        .build();
+
+                assertThrows(StartIsAfterEndException.class, () -> feedService
+                        .getFeedStats(USER_ID, statRequestParamDto));
+            }
+
+            @DisplayName("타입이 date이고 두 날짜 간의 차이가 30을 초과하면 예외 발생")
+            @Test
+            void throwDateRangeExceeds30Days() {
+                StatRequestParamDto statRequestParamDto = StatRequestParamDto.builder()
+                        .type("date")
+                        .start(LocalDate.of(2023, 10, 15))
+                        .end(LocalDate.of(2023, 11, 15))
+                        .build();
+
+                assertThrows(DateRangeExceeds30Days.class, () -> feedService
+                        .getFeedStats(USER_ID, statRequestParamDto));
+            }
+
+            @DisplayName("타입이 hour이고 두 날짜 간의 차이가 7을 초과하면 예외 발생")
+            @Test
+            void throwDateRangeExceeds7Days() {
+                StatRequestParamDto statRequestParamDto = StatRequestParamDto.builder()
+                        .type("hour")
+                        .start(LocalDate.of(2023, 10, 15))
+                        .end(LocalDate.of(2023, 10, 23))
+                        .build();
+
+                assertThrows(DateRangeExceeds7Days.class, () -> feedService
+                        .getFeedStats(USER_ID, statRequestParamDto));
+            }
+        }
     }
-
-    @DisplayName("피드 상세 조회시 조회수 1 증가")
-    @Test
-    void find_feed_detail_update_view_count() {
-        Feed feed = Feed.builder()
-            .type(FEED_TYPE)
-            .title(FEED_TITLE)
-            .content(FEED_CONTENT)
-            .contentId(FEED_CONTENT_ID)
-            .build();
-
-        doReturn(Optional.of(feed)).when(feedRepository).findById(FEED_ID);
-
-        final FeedDetailResponseDto result = feedService.findFeedDetail(FEED_ID);
-
-        assertThat(result.getViewCount()).isEqualTo(FEED_DEFAULT_VIEW_COUNT + 1);
-    }
-
 }
