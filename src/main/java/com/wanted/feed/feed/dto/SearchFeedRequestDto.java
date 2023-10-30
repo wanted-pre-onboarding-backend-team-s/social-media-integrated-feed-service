2720414b8e9b76293ea256cde33d236eba05810e
min050410 <min050410@gmail.com>
2023-10-30 13:20:18 +0900

    Feature: SearchFeedRequestDto 작성

--- src/main/java/com/wanted/feed/feed/dto/SearchFeedRequestDto.java
+++ src/main/java/com/wanted/feed/feed/dto/SearchFeedRequestDto.java
@@ -1,5 +1,9 @@
 package com.wanted.feed.feed.dto;
 
+import com.wanted.feed.common.validator.ValidEnum;
+import com.wanted.feed.feed.domain.enums.FeedOrderByType;
+import com.wanted.feed.feed.domain.enums.FeedSearchByType;
+import com.wanted.feed.feed.domain.enums.SortDirectionType;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,11 +16,23 @@ import lombok.Setter;
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class SearchFeedRequestDto {
 
+    private String hashtag;
+
     private String type;
-    private String order_by;
-    private String search_by;
+
+    @ValidEnum(enumClass = FeedOrderByType.class)
+    private FeedOrderByType order_by;
+
+    @ValidEnum(enumClass = FeedSearchByType.class)
+    private FeedSearchByType search_by;
+
+    @ValidEnum(enumClass = FeedSearchByType.class)
+    private SortDirectionType sort_direction;
+
     private String search;
+
     private Integer page_count;
+
     private Integer page;
 
 }
